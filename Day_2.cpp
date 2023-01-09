6fcca504b65d38a92ab9ca8407de2897e1662968
Yogesh Patel <113939209+Yogesh-Patel01@users.noreply.github.com>
2023-01-10 01:04:42 +0530

    Update and rename Day #2 to Day_2.cpp

--- Day_2.cpp
+++ Day_2.cpp
@@ -1,10 +1,11 @@
-:::: Question 1 ::::
-Remove vowels from string
+// :::: Question 1 ::::
+// Remove vowels from string
 
-problem statement:::
-https://practice.geeksforgeeks.org/problems/remove-vowels-from-string1446/1?fbclid=IwAR25-PTzc2bg5pvq_3AvFsxaCQ7kjDG3Rqj6GwAqzGCE4QVeN84vutb75nc
+// problem statement:::
+// https://practice.geeksforgeeks.org/problems/remove-vowels-from-string1446/1?fbclid=IwAR25-PTzc2bg5pvq_3AvFsxaCQ7kjDG3Rqj6GwAqzGCE4QVeN84vutb75nc
 
-:::: Solution ::::
+// :::: Solution ::::
+
 string removeVowels(string s){
 	    string ans = "";
 	    for(char x: s){
