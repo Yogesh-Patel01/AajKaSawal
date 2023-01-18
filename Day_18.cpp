3bff057227dad511befeeedddf3ec794dc85dacb
Yogesh Patel <113939209+Yogesh-Patel01@users.noreply.github.com>
2023-01-18 22:11:35 +0530

    Update Day_18.cpp

--- Day_18.cpp
+++ Day_18.cpp
@@ -1,7 +1,24 @@
 // :::: Question 1 ::::
-// Sqrt(x)
+// reate Target Array in the Given Order
 
 // problem statement:::
-// https://leetcode.com/problems/sqrtx/
+// https://leetcode.com/.../create-target-array-in-the.../
+
+// :::: Solution ::::
+
+vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+            ans.insert(ans.begin() + index[i], nums[i]);
+        return ans;
+    }
+
+
+
+// :::: Question 1 ::::
+// reate Target Array in the Given Order
+
+// problem statement:::
+// https://leetcode.com/.../create-target-array-in-the.../
 
 // :::: Solution ::::
