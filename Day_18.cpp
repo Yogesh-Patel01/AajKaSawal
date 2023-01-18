6552eca0e590656d1aad76e49a09523de55b1a98
Yogesh Patel <113939209+Yogesh-Patel01@users.noreply.github.com>
2023-01-18 22:30:15 +0530

    Update Day_18.cpp

--- Day_18.cpp
+++ Day_18.cpp
@@ -13,12 +13,40 @@ vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
         return ans;
     }
 
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-// :::: Question 1 ::::
-// reate Target Array in the Given Order
+// :::: Question 2 ::::
+// Lemonade Change
 
 // problem statement:::
-// https://leetcode.com/.../create-target-array-in-the.../
+// https://leetcode.com/problems/lemonade-change/description/
 
 // :::: Solution ::::
+
+bool lemonadeChange(vector<int>& bills) {
+        if(bills[0] != 5)
+            return false;
+        int five = 0;
+        int ten = 0;
+        for(auto x: bills){
+            if(x == 5)
+                five++;
+            else if(x == 10){
+                if(five < 1)
+                    return false;
+                ten++;
+                five--;
+            }
+            else{
+                if(ten > 0 && five > 0){
+                    ten--;
+                    five--;
+                }
+                else if(five > 2)
+                    five -= 3;
+                else
+                    return false;
+            }   
+        }
+        return true;
+    }
