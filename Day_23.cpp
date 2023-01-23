502f68be8681b22d8855f2a49c08edbe739d960a
Yogesh Patel <113939209+Yogesh-Patel01@users.noreply.github.com>
2023-01-23 23:13:11 +0530

    Update Day_23.cpp

--- Day_23.cpp
+++ Day_23.cpp
@@ -20,3 +20,35 @@ int maxLen(vector<int>&a, int n){
         }
         return len;
     }
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// :::: Question 2 ::::
+// Spiral travarsal of matrix
+
+// problem statement:::
+// https://leetcode.com/problems/spiral-matrix/description/?fbclid=IwAR0zxxqUCAYxu4b_uqzYb2v5Ab18BTfNrhkrtG73GNHM5bFk-DKwKdY60vM
+
+// :::: Solution ::::
+
+vector<int> spiralOrder(vector<vector<int>>& mat) {
+        vector<int> ans;
+        int right = mat[0].size(), left = 0, top = 0, down = mat.size();
+        while(left < right && top < down){
+            for(int i=left;i<right;i++)
+                ans.push_back(mat[top][i]);
+            top++;
+            for(int i=top;i<down;i++)
+                ans.push_back(mat[i][right-1]);
+            right--;
+            if(!(left < right && top < down))
+                break;
+            for(int i=right-1;i>left-1;i--)
+                ans.push_back(mat[down-1][i]);
+            down--;
+            for(int i=down-1;i>top-1;i--)
+                ans.push_back(mat[i][left]);
+            left++;
+        }
+        return ans;
+    }
