7ebb99e8ac33fc818a97d5e756227aee5dfb7aae
Yogesh Patel <113939209+Yogesh-Patel01@users.noreply.github.com>
2023-01-27 17:11:22 +0530

    Update Day_27.cpp

--- Day_27.cpp
+++ Day_27.cpp
@@ -2,7 +2,7 @@
 // 643. Maximum Average Subarray I
 
 // problem statement:::
-// https://leetcode.com/.../substrings-of-size-three-with.../
+// https://leetcode.com/.../maximum-average.../description/
 
 // :::: Solution ::::
 
@@ -28,9 +28,35 @@ double findMaxAverage(vector<int>& num, int k) {
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 
 // :::: Question 2 ::::
-// 643. Maximum Average Subarray I
+// 345. Reverse Vowels of a String
 
 // problem statement:::
-// https://leetcode.com/.../substrings-of-size-three-with.../
+// https://leetcode.com/problems/reverse-vowels-of-a-string/
 
 // :::: Solution ::::
+
+class Solution {
+    bool isvowel(char c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            return true;
+        return false;
+    }
+public:
+    string reverseVowels(string s) {
+        int n = s.size();
+        int start = 0, end = n - 1;
+        while(start < end){
+            if( isvowel(tolower(s[start])) && isvowel(tolower(s[end])) ){
+                char temp = s[start];
+                s[start] = s[end];
+                s[end] = temp;
+                start++, end--;
+            }
+            if(!isvowel(tolower(s[start])))
+                start++;
+            if(!isvowel(tolower(s[end])))
+                end--;
+        }
+        return s;
+    }
+};
