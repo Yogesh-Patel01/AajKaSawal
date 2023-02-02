0ae3251020a6d5a67130c9fd14b647b1804698c2
Yogesh Patel <113939209+Yogesh-Patel01@users.noreply.github.com>
2023-02-02 23:53:29 +0530

    Day 33

--- Day_33.cpp
+++ Day_33.cpp
@@ -22,3 +22,42 @@ public:
         return true;
     }
 };
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// :::: Question 2 ::::
+// 81. Search in Rotated Sorted Array II
+
+// problem statement:::
+// https://leetcode.com/.../search-in-rotated-sorted-array-ii/
+
+// :::: Solution ::::
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target)
+                return true;
+            if(nums[start] == nums[mid] && nums[end] == nums[mid]){
+                start++, end--;
+            }
+            else if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target < nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            else{
+                if(nums[end] >= target && target > nums[mid])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+
+        }
+        return false;
+    }
+};
